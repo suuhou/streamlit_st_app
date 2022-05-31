b8ff1b3c6856f19d65e205b296e05417f16ad707
suuhou <suuhou1108@gmail.com>
2022-05-31 11:58:12 +0900

    11

--- streamlit_learn.py
+++ streamlit_learn.py
@@ -25,8 +25,8 @@ if add_selectbox == "watch a demo":
     "---"
     r1c1, r1c2, r1c3, r1c4, r1c5, r1c6 = st.columns([1,1,1,1,1,1])
     with r1c1:
-        #st.image(load_local_image(r'http://cz.coder17.com/suuhou/test_convnextv2_nopaper/images/0001_fake_B.png'))
-        st.image("images/fake_0026.png")
+        st.image(load_local_image(r'http://cz.coder17.com/suuhou/test_convnextv2_nopaper/images/0001_fake_B.png'))
+        #st.image("images/fake_0026.png")
         st.image("images/fake_0026.png")
         st.image("images/fake_0038.png")
     with r1c2:
